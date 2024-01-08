379559e6f132d81d38c4f38dced477b11284313a
PlutoWooSeok <plutoxxx1014@gmail.com>
2024-01-08 11:22:22 +0900

    style: 파일명 변경

---
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,14 @@
+package hello.hellospring.repository;
+
+import org.junit.jupiter.api.Test;
+
+class MemoryMemberRepository {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @Test
+    public void save(){
+        Member menber = new Member();
+        
+    }
+}
